ed88e522310e3a7306e28e15aea25edb16113aa0
bharath <bharathkumar1807@gmail.com>
2021-05-21 00:50:17 +0530

    added output for app file

--- src/main/java/Academy/E2Eworkflow/App.java
+++ src/main/java/Academy/E2Eworkflow/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Bharath kumar" );
+        System.out.println( "Bharath kumaradded code" );
     }
 }

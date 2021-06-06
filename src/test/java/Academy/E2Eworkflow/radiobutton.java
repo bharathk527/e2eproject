e6946b6118515fa9291bbeda28ca2a0f14bd117b
bharath <bharathkumar1807@gmail.com>
2021-06-06 21:52:38 +0530

    radiobutton

---
+++ src/test/java/Academy/E2Eworkflow/radiobutton.java
@@ -0,0 +1,16 @@
+package Academy.E2Eworkflow;
+
+import java.io.IOException;
+
+import org.testng.annotations.BeforeTest;
+
+public class radiobutton extends base {
+
+	
+@BeforeTest
+	
+	public void initialize() throws IOException {
+		
+		driver=initializedriver();
+	}
+}
